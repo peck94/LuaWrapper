be530aa902554bb452fd9e8e9ed6e49524c67b23
Jonathan Peck <peckjonathan@gmail.com>
2015-09-06 19:57:45 +0200

    goto lol

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int main() {
 	cout << "Test Lua embedding" << endl;
+	bool var1;
+	int var2;
+	double var3;
+	float var4;
+	string var5;
 
 	cout << "Init Lua" << endl;
 	LuaWrapper *lw = new LuaWrapper();
@@ -15,10 +20,29 @@ int main() {
 	}
 
 	cout << "Set global vars" << endl;
-	lw->setGlobal("var1", true); // bool
-	lw->setGlobal("var2", 1); // int
-	lw->setGlobal("var3", 2.3f); // double
-	lw->setGlobal("var4", string{"yolo"}); // string
+	var1 = true;
+	var2 = 1;
+	var3 = 2.3f;
+	var4 = 2.3f;
+	var5 = "yolo";
+
+	lw->setGlobal("var1", var1);
+	lw->setGlobal("var2", var2);
+	lw->setGlobal("var3", var3);
+	lw->setGlobal("var4", var4);
+	lw->setGlobal("var5", var5);
+
+	cout << "Read global vars" << endl;
+	lw->getGlobal("var1", &var1);
+	cout << "var1 = " << var1 << endl;
+        lw->getGlobal("var2", &var2);
+	cout << "var2 = " << var2 << endl;
+        lw->getGlobal("var3", &var3);
+	cout << "var3 = " << var3 << endl;
+        lw->getGlobal("var4", &var4);
+	cout << "var4 = " << var4 << endl;
+        lw->getGlobal("var5", &var5);
+	cout << "var5 = " << var5 << endl;
 
 cleanup:
 	cout << "Clean-up" << endl;
